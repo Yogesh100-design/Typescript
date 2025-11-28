fcd942b75f22062958cd08bbea41318ac4d0c3c2
Yogesh chavan <yogeshchavan1209@gmail.com>
2025-11-28 19:54:46 +0530

    Learning TypeScript

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 12;
+function ram() {
+    console.log("jay shree ram");
+}
+// There are 2 types of datatypes in TS i.e Primitive and reaference
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,7 @@
+var a = 12;
+
+function ram(){
+    console.log("jay shree ram")
+}
+
+// There are 2 types of datatypes in TS i.e Primitive and reaference
