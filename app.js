cf580d8d115a6cb85cb8d628440e010e86dfa8f1
Yogesh chavan <yogeshchavan1209@gmail.com>
2025-11-29 16:34:13 +0530

    learn: datatypes, arrays, enum

--- app.js
+++ app.js
@@ -5,4 +5,29 @@ function ram() {
     console.log("jay shree ram");
 }
 // There are 2 types of datatypes in TS i.e Primitive and reaference
+//ARRAR
+let arr = [1, 2, 3, "ram"];
+let arr1 = [1, 2, 3];
+// let arr1: number[] = [1,2,3,"ram"]; wrong
+// TUPLE
+let arr2 = [1, "ram"];
+// let arr3 : [number,string] = ["ram",1] wrong
+
+//enumeration
+var Userrole;
+(function (Userrole) {
+    Userrole["ADMIN"] = "admin";
+    Userrole["GUEST"] = "guest";
+    Userrole["SUPERADMIN"] = "superadmin";
+})(Userrole || (Userrole = {}));
+var Statuscode;
+(function (Statuscode) {
+    Statuscode[Statuscode["SUCCESS"] = 200] = "SUCCESS";
+    Statuscode[Statuscode["NOTFOUND"] = 404] = "NOTFOUND";
+    Statuscode[Statuscode["UNAUTHORIZED"] = 401] = "UNAUTHORIZED";
+})(Statuscode || (Statuscode = {}));
+//Usage
+Userrole.ADMIN;
+Userrole.GUEST;
+Userrole.SUPERADMIN;
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -5,3 +5,34 @@ function ram(){
 }
 
 // There are 2 types of datatypes in TS i.e Primitive and reaference
+
+//ARRAR
+let arr =[1,2,3,"ram"];
+
+let arr1: number[] = [1,2,3];
+// let arr1: number[] = [1,2,3,"ram"]; wrong
+
+// TUPLE
+
+let arr2 : [number,string] = [1,"ram"]
+// let arr3 : [number,string] = ["ram",1] wrong
+
+//enumeration
+
+enum Userrole  {
+     ADMIN = "admin",
+     GUEST = "guest",
+     SUPERADMIN = "superadmin"
+}
+
+enum Statuscode {
+    SUCCESS = 200,
+    NOTFOUND = 404,
+    UNAUTHORIZED = 401
+}
+
+//Usage
+Userrole.ADMIN
+Userrole.GUEST
+Userrole.SUPERADMIN 
+
